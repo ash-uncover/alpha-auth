25e368733492111d7fea81736d2efdeb86abd784
ash-uncover <ash.uncover@gmail.com>
2020-11-25 21:02:36 +0100

    added the express stuff

---
+++ webpack.config.dev.js
@@ -0,0 +1,16 @@
+/* eslint-disable */
+
+const { merge } = require('webpack-merge')
+const common = require('./webpack.config.common.js')
+
+module.exports = merge(common, {
+  mode: 'development',
+
+  devtool: 'source-map',
+
+  devServer: {
+    contentBase: './dist',
+    historyApiFallback: true,
+    disableHostCheck: true
+  },
+})
